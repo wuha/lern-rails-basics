fd77ce4a5037da7426d2140f6797e8c44f8d31a5
Simon Goetz <simon.goetz@me.com>
2013-09-13 12:25:59 +0200

    Create new Testfiles

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LernRails::Application.config.secret_key_base = '9be6753978a08142213b8768bfc621e0d0fb05829a3c57ba30fd630cf49ebbd27ec3169cff99467ef25702b9ddcdae96752d807afc7ea49e7ac8ff95b06a1bef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LernRails::Application.config.session_store :cookie_store, key: '_lern-rails_session'
